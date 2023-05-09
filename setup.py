5f6d7b3a73d1379c3e8c0c5ffe0cab3bfe4e5999
“Juan <j.fernandez@fz-juelich.de>
2023-05-09 17:18:46 +0200

    Add new files to repository

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Project for the DDE II subject for the Masters Degree in Mechanical Engineering',
+    author='Juan_Fernandez',
+    license='MIT',
+)
